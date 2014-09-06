809ff622bb75e3db1170a85baf1569877a644b03
Devin Holland <holl4332@kettering.edu>
2014-09-06 15:31:42 -0400

    test

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
       $cookie_file_path = "cookies.txt";
       $ch = curl_init();
       curl_setopt($ch, CURLOPT_URL,$LOGINURL);
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
       curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
       curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_file_path);
       curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file_path);
