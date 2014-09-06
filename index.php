056be810f8702482c7c8fbc2b5c23f25b3da6012
Devin Holland <holl4332@kettering.edu>
2014-09-06 12:18:56 -0400

    test

--- index.php
+++ index.php
@@ -17,13 +17,16 @@
   <?php
     // Get cURL resource
     $curl = curl_init();
+    $fields = array ('sid' => urlencode("holl4332"), 'PIN' => urlencode("123456789"));
+    foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+    rtrim($fields_string, '&');
     // Set some options - we are passing in a useragent too here
     curl_setopt($curl, CURLOPT_URL, "http://jweb.kettering.edu/cku1/twbkwbis.P_ValLogin");
-    curl_setopt($curl, CURLOPT_POST, 1);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, "sid=holl4332&PIN=123456789");
+    curl_setopt($curl, CURLOPT_POST, count($fields));
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $fields_string);
     curl_setopt($curl, CURLOPT_COOKIEJAR, "cookie.txt");
     curl_setopt($curl, CURLOPT_COOKIESESSION, true);
-    curl_setopt($curl, CURLOPT_COOKIEFILE, "var/www/ip4.x/file/tmp");
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
     // Send the request & save response to $resp
 
