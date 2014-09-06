d6a005a05db04ad7f2f10bf952c9448dcd663e8e
Devin Holland <holl4332@kettering.edu>
2014-09-06 14:15:56 -0400

    test

--- index.php
+++ index.php
@@ -16,37 +16,31 @@
     <input style="color:#d9d9d9; background-color:#111; border-color:#111; text-align:left;" type='text' id='messageInput' class="ui-autocomplete-input ui-button ui-corners-all" placeholder='Message'>
   
     <?php
-/*
-This script is an example of using curl in php to log into on one page and 
-then get another page passing all cookies from the first page along with you.
-If this script was a bit more advanced it might trick the server into 
-thinking its netscape and even pass a fake referer, yo look like it surfed 
-from a local page.
-*/
+      $LOGINURL = "http://jweb.kettering.edu/cku1/twbkwbis.P_ValLogin";
+      $cookie_file_path = "cookies.txt";
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL,$LOGINURL);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+      curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_file_path);
+      curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file_path);
+      $result = curl_exec ($ch);
+      curl_close ($ch);
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_COOKIEJAR, "/tmp/cookieFileName");
-curl_setopt($ch, CURLOPT_URL,"http://jweb.kettering.edu/cku1/twbkwbis.P_ValLogin");
+      $POSTFIELDS = "sid=holl4332&PIN=21797721";
+      $ch = curl_init(); 
+        curl_setopt($ch, CURLOPT_URL,$LOGINURL);
+        curl_setopt($ch, CURLOPT_POST, 1); 
+        curl_setopt($ch, CURLOPT_POSTFIELDS,$POSTFIELDS); 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_file_path);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file_path);
+        $result = curl_exec ($ch);
+        curl_close ($ch); 
+        print $result;  
+    ?>
 
-ob_start();      // prevent any output
-curl_exec ($ch); // execute the curl command
-ob_end_clean();  // stop preventing output
-
-curl_close ($ch);
-unset($ch);
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-curl_setopt($ch, CURLOPT_COOKIEFILE, "/tmp/cookieFileName");
-curl_setopt($ch, CURLOPT_URL,"http://jweb.kettering.edu/cku1/twbkwbis.P_ValLogin");
-curl_setopt($ch, CURLOPT_POSTFIELDS, "sid=holl4332&PIN=21797721");
-
-$buf2 = curl_exec ($ch);
-
-curl_close ($ch);
-
-echo $buf2;
-?>
   </body>
   <script src="chat.js"></script>
 </html>
