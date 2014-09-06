f0dc3d3ab13161283d3c2050ebe53eb00c85e5e7
Devin Holland <holl4332@kettering.edu>
2014-09-06 14:17:36 -0400

    test

--- index.php
+++ index.php
@@ -21,7 +21,6 @@
       $ch = curl_init();
       curl_setopt($ch, CURLOPT_URL,$LOGINURL);
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
       curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_file_path);
       curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file_path);
       $result = curl_exec ($ch);
@@ -32,8 +31,7 @@
         curl_setopt($ch, CURLOPT_URL,$LOGINURL);
         curl_setopt($ch, CURLOPT_POST, 1); 
         curl_setopt($ch, CURLOPT_POSTFIELDS,$POSTFIELDS); 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_file_path);
         curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file_path);
         $result = curl_exec ($ch);
