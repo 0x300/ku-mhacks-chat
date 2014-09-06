78f283826200fba4831c7ff9b64fc515013b28fe
Devin Holland <holl4332@kettering.edu>
2014-09-06 10:36:33 -0400

    test php

--- index.php
+++ index.php
@@ -15,7 +15,14 @@
     <input style="color:#d9d9d9; background-color:#111; border-color:#111; text-align:left;" type='text' id='messageInput' class="ui-autocomplete-input ui-button ui-corners-all" placeholder='Message'>
   
   <?php
-    echo 'this';
+    $req = new HttpRequest('http://jweb.kettering.edu/cku1/twbkwbis.P_ValLogin', HttpRequest::METH_POST);
+    echo $req;
+    //$req->addPostFields(array('sid' => 'lind6441', 'PIN' => '1234'));
+    //try {
+    //    echo $req->send()->getBody();
+    //} catch (HttpException $ex) {
+    //    echo $ex;
+    //}
   ?>
 
   </body>
