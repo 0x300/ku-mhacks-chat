f8dbab4b03f7d762b1e8af923d7061a51200c45d
Devin Holland <holl4332@kettering.edu>
2014-09-06 20:56:05 -0400

    further testing

--- index.php
+++ index.php
@@ -60,7 +60,9 @@
           url: "php-proxy.php",
           data: {url: "https://jweb.kettering.edu/cku1/bwskfshd.P_CrseSchdDetl?term_in=201402"},
           success: function(resp){console.log(resp);}
-        });
+        }).fail(){
+          alert("failed..");
+        };
       });
     </script>
 
