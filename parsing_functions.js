9739afa1dc31bc4f6c40c5be85e2941ac62dab7b
Devin Holland <holl4332@kettering.edu>
2014-09-06 22:33:57 -0400

    Major change to parsing_functions

--- parsing_functions.js
+++ parsing_functions.js
@@ -1,17 +1,62 @@
 function parseSchedule(data) {
-	//gets bolded class name, e.x.: Microcomputers I - CE 320 - 01
-	var classNames = $(data).children('.captiontext');
-	
-	// get row of class='ddlabel' e.x.: CRN:	30082
-	var CRNs = $(data).children('.ddlabel');
-	
-	
-}
-
-function parseStudent(data) {
-	// get student name
+	var classesRef =  new Firebase("https://sizzling-heat-3782.firebaseio.com/Classes")
 	var header = $(data).children('.staticheaders');
-	var name = header.substring(10, header.length-9);
+	var name = header.html();
+	name = $.trim(name).substring(10,header.html().length-48)
+	var userRef = new Firebase("https://sizzling-heat-3782.firebaseio.com/Users");
+	var users;
+	var userFound = false;
+	var userID;
+	userRef.once("value", function(snapshot){
+		users = snapshot.val();
+		.each(users, function(index, userObject)
+		{
+			if(userObject.userName == name)
+			{
+				userFound = true;
+				userID = userObject.name();
+			}
+		});
 
+		if(userFound == false)
+		{
+			userRef.on("child_added", function(snapshot)
+			{
+				userID = snapshot.name(); 
+			});
+			userRef.push({userName: name});
+		}
+	});
+	//gets bolded class name, e.x.: Microcomputers I - CE 320 - 01
+	$(data).find('.captiontext').each(function()
+	{
+		var theClassName = $(this).html();
+		var classes;
+		var classFound = false;
+		if(!(theClassName == "Scheduled Meeting Times"))
+		{
 
+			classesRef.once("value", function(snapshot){
+				classes = snapshot.val();
+
+				$.each(classes, function(index, classObject){
+					if(theClassName == classObject.ClassName)
+					{
+						classFound = true;
+						userRef.once("value", function(snapshot)
+						{
+							snapshot.val().Classes.push({ClassName : theClassName, classKey : classObject.name() });
+						});
+					}
+				});
+
+				if(classFound == false)
+				{
+					userRef.once("value", function(snapshot){
+						snapshot.val()[userID].Classes.push(ClassName : theClassName, classKey : classesRef.push({ClassName : theClassName}).name());
+					});
+				}
+			});
+		}
+	});
 }
